58dc9063a1ebf00e6c38dd63d6a694471ce6434e
Cristea3051 <cristeavictor4@gmail.com>
2024-09-06 11:55:05 +0300

    dsadsada

--- src/test/java/Locators3.java
+++ src/test/java/Locators3.java
@@ -12,6 +12,9 @@ public class Locators3 {
 
     // Sibling - Child to parent traverse
    System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+
+   System.out.println(driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).getText());
+   
     
    driver.close();
 
---
+++ src/test/java/WindowActivities.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowActivities {
+
+
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+
+        // Face maximize la browser 
+        driver.manage().window().maximize();
+
+        driver.get("https://google.com/");
+
+        // Din acelasi browser, deschide un nou url specificat mai jos
+        driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+
+        // pentru ma intoarce inapoi pe url-ul de la care al facut navigate foolosesc: 
+        driver.navigate().back();
+
+        // Metoda pentru a se intoarce inapoi la navigate
+        driver.navigate().forward();
+        
+        driver.close();
+    }
+}
