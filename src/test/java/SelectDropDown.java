891e6f4727f01ac3c20e0fed0d8ca6d6f2455b08
Cristea3051 <cristeavictor4@gmail.com>
2024-09-06 15:48:00 +0300

    Am adaugat interactiune cu dropdown select

---
+++ src/test/java/SelectDropDown.java
@@ -0,0 +1,60 @@
+    import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+    import org.openqa.selenium.support.ui.Select;
+    
+    public class SelectDropDown {
+    
+    
+        public static void main(String[] args) throws InterruptedException{
+    
+            WebDriver driver = new ChromeDriver();
+    
+            // Face maximize la browser 
+            driver.manage().window().maximize();
+    
+            driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+            // Dropdown with select tag in momentul cand un select dropdown este static
+            WebElement staticDropdown =  driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency"));
+            Select dropdown = new Select(staticDropdown);
+            dropdown.selectByIndex(3);
+            System.out.println(dropdown.getFirstSelectedOption().getText());
+            dropdown.selectByVisibleText("AED");
+            System.out.println(dropdown.getFirstSelectedOption().getText());
+            dropdown.selectByValue("INR");
+            System.out.println(dropdown.getFirstSelectedOption().getText());
+
+            // Cum interactionez cu un dropdown na un nivel mai avansat
+            driver.findElement(By.id("divpaxinfo")).click();
+            System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+            Thread.sleep(2000);
+            int i = 1;
+            while (i<5) {
+                driver.findElement(By.id("hrefIncAdt")).click();
+                i++;
+                
+            } 
+            for(int s =1; s<5; s++) {
+                driver.findElement(By.id("hrefIncAdt")).click();
+
+            }
+            driver.findElement(By.id("btnclosepaxoption")).click();
+            System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+            // Dynamic dropdowns how to interact
+            driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+            driver.findElement(By.xpath("//a[@value='BLR']")).click();
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("(//a[@value=\"MAA\"])[2]")).click();
+
+
+
+            // (//a[@value="MAA"])[2]
+
+            
+
+            // driver.close();
+        }
+    }
