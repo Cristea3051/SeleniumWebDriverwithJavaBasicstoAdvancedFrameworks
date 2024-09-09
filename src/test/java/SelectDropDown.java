b7dbc4797c78a01cba220418a4503d3431eb5154
Cristea3051 <cristeavictor4@gmail.com>
2024-09-09 17:06:09 +0300

    dsasasd

--- src/test/java/SelectDropDown.java
+++ src/test/java/SelectDropDown.java
@@ -1,4 +1,6 @@
-    import org.openqa.selenium.By;
+    import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -47,13 +49,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
             driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
             driver.findElement(By.xpath("//a[@value='BLR']")).click();
             Thread.sleep(2000);
-            driver.findElement(By.xpath("(//a[@value=\"MAA\"])[2]")).click();
 
-            //div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']
+            // driver.findElement(By.xpath("(//a[@value=\"MAA\"])[2]")).click();
 
+            driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
-            // (//a[@value="MAA"])[2]
+          // (//a[@value="MAA"])[2]
 
+        //   Autosigestive dropdown or dynamic search dropdown
+
+        driver.findElement(By.id("autosuggest")).sendKeys("aus");
+        Thread.sleep(4000);
+        List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+        for(WebElement option :options) {
+         if( option.getText().equalsIgnoreCase("Austria")){
+            option.click();
+            break;
+         }
+
+        }
             // driver.close();
         }
     }
