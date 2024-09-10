ed73c2894b99af5b26a9cca2b77b59d98425a482
Cristea3051 <cristeavictor4@gmail.com>
2024-09-10 14:02:40 +0300

    dsadsa

--- src/test/java/SelectDropDown.java
+++ src/test/java/SelectDropDown.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
     import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
     
     public class SelectDropDown {
     
@@ -28,7 +29,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
             dropdown.selectByValue("INR");
             System.out.println(dropdown.getFirstSelectedOption().getText());
 
-            // Cum interactionez cu un dropdown na un nivel mai avansat
+            // Select roundtrip
+            driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+
+
+            // Cum interactionez cu un dropdown La un nivel mai avansat
             driver.findElement(By.id("divpaxinfo")).click();
             System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
             Thread.sleep(2000);
@@ -45,19 +50,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
             driver.findElement(By.id("btnclosepaxoption")).click();
             System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 
+            // Assert equals in testng
+            Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "9 Adult");
+
+
+
+
             // Dynamic dropdowns how to interact
             driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
             driver.findElement(By.xpath("//a[@value='BLR']")).click();
             Thread.sleep(2000);
-
-            // driver.findElement(By.xpath("(//a[@value=\"MAA\"])[2]")).click();
-
             driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
-          // (//a[@value="MAA"])[2]
+            // Calendar select data
+            driver.findElement(By.cssSelector(".ui-datepicker-days-cell-over.ui-datepicker-today")).click();
+
+            // Select roundtrip
+            driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+            System.out.println(driver.findElement(By.name("ctl00$mainContent$view_date2")).isEnabled() + " asta la calendar");
+
+
+
 
         //   Autosigestive dropdown or dynamic search dropdown
-
         driver.findElement(By.id("autosuggest")).sendKeys("aus");
         Thread.sleep(4000);
         List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
@@ -68,6 +83,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
          }
 
         }
-            // driver.close();
+        // Select and verify checkboxes
+        // Assert false in testng, va compara daca nu este selectat cheboxul va da false si va fi egal cu asertul din test ng ceea ce va face ca testul sa fie passed
+        Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Assert.assertFalse(false);
+        // Va printa in colosla true sau false in dependenta daca este sau nu apasat acel checkbox
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        // Va da click pe checkbox
+        driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
+       // Va printa in colosla true sau false in dependenta daca este sau nu apasat acel checkbox
+        System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+        // asert true/false daca a fost sau nu apasat chechboxul
+        Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+        Assert.assertTrue(true);
+
+        // Count the number of all checkboxes in pagina
+        System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+
+            driver.close();
         }
     }
