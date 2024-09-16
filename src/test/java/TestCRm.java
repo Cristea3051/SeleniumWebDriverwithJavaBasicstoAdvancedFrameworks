79d00e6f8f4dac4aac97a0eaf5a6e786e9bb7d11
Cristea3051 <cristeavictor4@gmail.com>
2024-09-16 14:55:10 +0300

    Am facut ceva formatare in TestCRM, deci acum se vor putea apela orice parametri dupa numele coloanei care vreau s-o ascund etc

--- src/test/java/TestCRm.java
+++ src/test/java/TestCRm.java
@@ -4,15 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import java.util.List;
 
 import java.time.Duration;
 
-
 public class TestCRm {
 
-    public static void main(String[] args) throws InterruptedException{
-          WebDriver driver = new ChromeDriver();
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
@@ -29,43 +27,44 @@ public class TestCRm {
 
         driver.findElement(By.className("phpdebugbar-close-btn")).click();
 
-        // driver.findElement(By.cssSelector(".fa-plus-circle")).click();
+        driver.findElement(By.cssSelector(".fa-plus-circle")).click();
 
-        // driver.findElement(By.id("create_name")).sendKeys("21.226.116.124:4444");
-        
-        // driver.findElement(By.id("create_username")).sendKeys("ProxyAutoUsername");
+        driver.findElement(By.id("create_name")).sendKeys("21.226.116.124:4444");
 
-        // driver.findElement(By.id("create_password")).sendKeys("ProxyAutoPass");
+        driver.findElement(By.id("create_username")).sendKeys("ProxyAutoUsername");
 
-        // WebElement select = driver.findElement(By.cssSelector("label[for='create_proxy_types'] ~ select[name='proxy_type']"));
-        // Select type = new Select(select);
-        // type.selectByValue("Residential");
+        driver.findElement(By.id("create_password")).sendKeys("ProxyAutoPass");
 
-        // WebElement source = driver.findElement(By.cssSelector("input[data-modal-field-id='create_source_id']"));
-        // Thread.sleep(4000);
-        // source.sendKeys("ip");
-        // driver.switchTo().activeElement().sendKeys(Keys.ARROW_DOWN);
-        // driver.switchTo().activeElement().sendKeys(Keys.ENTER);
+        WebElement select = driver
+                .findElement(By.cssSelector("label[for='create_proxy_types'] ~ select[name='proxy_type']"));
+        Select type = new Select(select);
+        type.selectByValue("Residential");
 
-        // driver.findElement(By.id("create-accounts-proxy-button")).click();
+        WebElement source = driver.findElement(By.cssSelector("input[data-modal-field-id='create_source_id']"));
+        Thread.sleep(4000);
+        source.sendKeys("ip");
+        driver.switchTo().activeElement().sendKeys(Keys.ARROW_DOWN);
+        driver.switchTo().activeElement().sendKeys(Keys.ENTER);
 
+        driver.findElement(By.id("create-accounts-proxy-button")).click();
 
-        // driver.navigate().refresh();
+        driver.navigate().refresh();
 
-        // driver.findElement(By.xpath("//td[text()='21.226.116.124:4444']//preceding-sibling::td//input[@class='form-check-input']")).click();
+        driver.findElement(
+                By.xpath("//td[text()='21.226.116.124:4444']//preceding-sibling::td//input[@class='form-check-input']"))
+                .click();
 
-        // Thread.sleep(2000);
+        Thread.sleep(2000);
 
-        // driver.findElement(By.cssSelector(".fa-trash-alt")).click();
+        driver.findElement(By.cssSelector(".fa-trash-alt")).click();
 
-        // driver.findElement(By.cssSelector(".swal2-confirm")).click();
+        driver.findElement(By.cssSelector(".swal2-confirm")).click();
 
+        driver.findElement(By.cssSelector(".fa-table")).click();
 
-driver.findElement(By.cssSelector(".fa-table")).click();
+        driver.findElement(By.id("setting-name")).sendKeys("ProxyAutoTableSetting");
 
-driver.findElement(By.id("setting-name")).sendKeys("ProxyAutoTableSetting");
-
-SettingsHelper settingsHelper = new SettingsHelper(driver);
+        SettingsHelper settingsHelper = new SettingsHelper(driver);
 
         // Selectează multiple valori
         String[] valuesToSelect = { "Owner", "Updated At", "Created At" };
@@ -75,14 +74,13 @@ SettingsHelper settingsHelper = new SettingsHelper(driver);
         settingsHelper.clickNavigationButton("fa fa-arrow-circle-right");
 
         // Mută elementele
-        settingsHelper.selectMultipleValuesByValue(new String[]{ "Mediabuyers" });
+        settingsHelper.selectMultipleValuesByValue(new String[] { "Mediabuyers" });
         settingsHelper.moveElements("fa fa-arrow-circle-up", 4);
 
         // Mută alte elemente
-        settingsHelper.selectMultipleValuesByValue(new String[]{ "Source" });
+        settingsHelper.selectMultipleValuesByValue(new String[] { "Source" });
         settingsHelper.moveElements("fa fa-arrow-circle-up", 4);
 
     }
 }
-
 
