d761caefdce51354c73015e71694b5ccb20e6ebf
Cristea3051 <cristeavictor4@gmail.com>
2024-09-16 12:22:41 +0300

    Am terminat metoda add to cart in selenium. Este utila atunci cand sunt prea multe elemente care au acelasi loctor

--- src/test/java/AddToCard.java
+++ src/test/java/AddToCard.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class AddToCardTest {
+public class AddToCard {
 
     public static void main(String[] args) throws InterruptedException {
 
