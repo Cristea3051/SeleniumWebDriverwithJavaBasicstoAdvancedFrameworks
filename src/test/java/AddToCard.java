57cb53c906b1efb22a05babb8b8d7af70f9ed72c
Cristea3051 <cristeavictor4@gmail.com>
2024-09-17 11:17:12 +0300

    Explicti wait partea, am lasat un comentariu cu explicatie

--- src/test/java/AddToCard.java
+++ src/test/java/AddToCard.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,14 +13,27 @@ public class AddToCard {
 
         WebDriver driver = new ChromeDriver();
 
+        /* implicitlyWait seteaza timul global
+           Lucreaza in felul urmator: daca elementul pe pagina nu este gasint, 
+           selenium asteapta cateva secunde in acest caz 10 si daca in aceste secunde elementul nu apare,
+           selenium face fail la test */ 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         String[] itemsNeeded = { "Cucumber", "Cauliflower" ,"Brocolli", "Beetroot", "Carrot", "Tomato", "Beans" };
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
 
-        Thread.sleep(3000);
-
         addItems(driver, itemsNeeded);
 
+     driver.findElement(By.cssSelector("img[alt='Cart']")).click();   
+
+     driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+
+     driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacedemy");
+
+     driver.findElement(By.cssSelector("button.promoBtn")).click();
+
+     System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
     }
 
     public static void addItems(WebDriver driver, String[] itemsNeeded)
