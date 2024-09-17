539ccdd2d83f44fd1ea06457867f8df71cb93532
Cristea3051 <cristeavictor4@gmail.com>
2024-09-17 11:35:08 +0300

    M-am lamurit cu explicit wait, dar mi se pare ca metoda de implciti e putin mai buna. Am lasat cu explicit si am comentat tot ce era legat de explicit. In ambele cazuri testul ruleaza corect

--- src/test/java/AddToCard.java
+++ src/test/java/AddToCard.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AddToCard {
 
@@ -13,15 +15,19 @@ public class AddToCard {
 
         WebDriver driver = new ChromeDriver();
 
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
         /* implicitlyWait seteaza timul global
            Lucreaza in felul urmator: daca elementul pe pagina nu este gasint, 
            selenium asteapta cateva secunde in acest caz 10 si daca in aceste secunde elementul nu apare,
            selenium face fail la test */ 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         String[] itemsNeeded = { "Cucumber", "Cauliflower" ,"Brocolli", "Beetroot", "Carrot", "Tomato", "Beans" };
 
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+        
+        Thread.sleep(3000);
 
         addItems(driver, itemsNeeded);
 
@@ -29,9 +35,20 @@ public class AddToCard {
 
      driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
 
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input.promoCode")));
+
      driver.findElement(By.cssSelector("input.promoCode")).sendKeys("rahulshettyacedemy");
 
      driver.findElement(By.cssSelector("button.promoBtn")).click();
+
+     /* ExplictiWait se foloseste exact ca espresia de mai jos si anume:
+        Declaram o variabila, in cazul de fata este wait. 
+        Dupa asta folosim expresia new WebDriverWait(driver, Duration.ofSeconds(10)) 
+        pentru a seta timpul de asteptare pentru pasul care urmeaza
+        Folosim variabila wait pentru a construi locatorul care vrem, iar el autmat va astepta
+       */
+    
+     wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
 
      System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
     }
