187bad48f4bad789c24c771337293585c694cd68
Cristea3051 <cristeavictor4@gmail.com>
2024-09-17 15:22:54 +0300

    dasdsa

--- src/test/java/FluentWait.java
+++ src/test/java/FluentWait.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.FluentWait;
+
 import java.time.Duration;
 
 public class FluentWait {
