7538128779b81675e41a6edad44ecaa3fa30d16d
Cristea3051 <cristeavictor4@gmail.com>
2024-09-19 12:15:30 +0300

    am adaugat niste comenatrii

--- src/test/java/WindowHandle.java
+++ src/test/java/WindowHandle.java
@@ -27,6 +27,16 @@ public class WindowHandle {
 
         driver.findElement(By.id("username")).sendKeys(email);
 
-
+/*Deci aici este prezenta o bucata de cod care face urmatoarele: 
+ * 1. Navigeaza la urlul dat
+ * 2. Apasa clicl pe un a tah care contine un url ce face sa se dechida un tab nou
+ * 3. Seteaza pentru slenium toate taburile disponibile in browser 
+ * 4. Formateaza si itereaza prin taburi
+ * 5. Le scoate pe fiecare disponibil sub forma de variabile pentru a naviga mai usor 
+ * 6. Face switch lui selenium la tabul urmator deschis. 
+ * 7. Prelucreaza elenetul din care vrem sa extragem textul: face spl;it si trim si scoate doar ceea de ce am nevoie, in cazul de fata email
+ * 8. Face switch la tabul principal, cel care s-a deschis pentru tima oara in driver get
+ * 9. Gasete inputul in care vrem sa inseram acel text de la pasul 7 si il insereaza
+ */
     }
 }
