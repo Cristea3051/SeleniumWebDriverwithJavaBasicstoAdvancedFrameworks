2caa6ac21107aa7a63b6c47442fbdb803d697b18
Cristea3051 <cristeavictor4@gmail.com>
2024-09-20 09:29:53 +0300

    dasdassdsa

--- src/test/java/FrameTest.java
+++ src/test/java/FrameTest.java
@@ -12,11 +12,19 @@ public class FrameTest {
 
         driver.get("https://jqueryui.com/droppable/");
 
+        /* Deci aici este mecanismul de drag and drop in frame.
+         * 1. Localizam frameul si il stocam intr-o variabila
+         * 2. Facem switch in acest frame. 
+         * 3. Localizam id-ul cu elementul cu care vrem sa facem drag & drop
+         * 4. Setam actionul 
+         * 5. Stocam elementele cu care vreau sa fac drag si in care vreau sa fac drop
+         * 6. Setez metoda prin care trebuie sa se faca drag & drop
+         */
+
         WebElement frame = driver.findElement(By.cssSelector("iframe.demo-frame"));
 
         driver.switchTo().frame(frame);
         driver.findElement(By.id("draggable"));
-
         Actions drag = new Actions(driver);
 
         WebElement source = driver.findElement(By.id("draggable"));
