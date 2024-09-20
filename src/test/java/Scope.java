eeb283bb741f1d45151fd4c672ac9d74bf12be07
Cristea3051 <cristeavictor4@gmail.com>
2024-09-20 13:01:51 +0300

    asdsadsadas

---
+++ src/test/java/Scope.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Scope {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+        // Print the links count on the page
+        System.out.println(driver.findElements(By.tagName("a")).size());
+
+        WebElement footerDriver = driver.findElement(By.id("gf-BIG")); /* Limit webdriver to element footer */
+
+        System.out.println(footerDriver.findElements(By.tagName("a")).size());
+
+        // Count first column links
+
+        WebElement columnFooterDriver =  footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+
+        System.out.println(columnFooterDriver.findElements(By.tagName("a")).size());
+
+          // Click on each link in the column and chech if pages are opening
+
+          for (int i=1; i<columnFooterDriver.findElements(By.tagName("a")).size(); i++){
+            columnFooterDriver.findElements(By.tagName("a")).get(i).click();
+          }
+
+    }
+}
