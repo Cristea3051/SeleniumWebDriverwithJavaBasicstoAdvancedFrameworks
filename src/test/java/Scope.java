bc5f389f82b20fbabfc0c751973e88e6070164db
Cristea3051 <cristeavictor4@gmail.com>
2024-09-25 14:04:00 +0300

    dasdsa

--- src/test/java/Scope.java
+++ src/test/java/Scope.java
@@ -9,46 +9,47 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Scope {
 
-    public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+//1. Give me the count of links on the page.
+		//2. Count of footer section-
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("http://qaclickacademy.com/practice.php");
+		
+		System.out.println(driver.findElements(By.tagName("a")).size());
+		
+		WebElement footerdriver=driver.findElement(By.id("gf-BIG"));// Limiting webdriver scope
+		
+		System.out.println(footerdriver.findElements(By.tagName("a")).size());
+		
+		//3-
+		WebElement coloumndriver=footerdriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
+		System.out.println(coloumndriver.findElements(By.tagName("a")).size());
+		
+		//4- click on each link in the coloumn and check if the pages are opening-
+		for(int i=1;i<coloumndriver.findElements(By.tagName("a")).size();i++)
+		{
+			
+			String clickonlinkTab=Keys.chord(Keys.CONTROL,Keys.ENTER);
+			
+			coloumndriver.findElements(By.tagName("a")).get(i).sendKeys(clickonlinkTab);
+			Thread.sleep(5000L);
+			
+		}// opens all the tabs
+		Set<String> abc=driver.getWindowHandles();//4
+		Iterator<String> it=abc.iterator();
+		
+		while(it.hasNext())
+		{
+			
+		   driver.switchTo().window(it.next());
+		   System.out.println(driver.getTitle());
+		
+		}
+			
+  }
 
-        WebDriver driver = new ChromeDriver();
-        
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-
-        // Print the links count on the page
-        System.out.println(driver.findElements(By.tagName("a")).size());
-
-        WebElement footerDriver = driver.findElement(By.id("gf-BIG")); /* Limit webdriver to element footer */
-
-        System.out.println(footerDriver.findElements(By.tagName("a")).size());
-
-        // Count first column links
-
-        WebElement columnFooterDriver =  footerDriver.findElement(By.xpath("//table/tbody/tr/td[1]/ul"));
-
-        System.out.println(columnFooterDriver.findElements(By.tagName("a")).size());
-
-          // Click on each link in the column and chech if pages are opening
-
-          for (int i=1; i<columnFooterDriver.findElements(By.tagName("a")).size(); i++){
-
-            String clickToOpenNewTab = Keys.chord(Keys.CONTROL,Keys.ENTER);
-
-            columnFooterDriver.findElements(By.tagName("a")).get(i).sendKeys(clickToOpenNewTab);
-
-            Thread.sleep(5000);
-
-            Set<String> abc = driver.getWindowHandles();
-
-            Iterator<String> it = abc.iterator();
-
-            while(it.hasNext()){
-
-              driver.switchTo().window(it.next());
-              System.out.println(driver.getTitle());
-              driver.close();
-            }
-          }
-
-    }
 }
