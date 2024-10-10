6f967d2f2f30c99b15cfa25671e1380ad5a4427e
Cristea3051 <cristeavictor4@gmail.com>
2024-10-10 12:40:37 +0300

    dsadsadsa

--- src/test/java/CRMTest/SGCampaigns.java
+++ src/test/java/CRMTest/SGCampaigns.java
@@ -1,6 +1,7 @@
 package CRMTest;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -35,27 +36,41 @@ public static void main(String[] args) throws InterruptedException {
         Thread.sleep(2000);
         
         driver.findElement(By.cssSelector("div[data-collapse='Delivered']")).click();
-        driver.findElement(By.cssSelector("input[placeholder='Delivered']")).click();
-
-        WebElement yearCalendar = driver
-                .findElement(By.cssSelector("button[class ='react-calendar__navigation__label']"));
-
-        yearCalendar.click();
         Thread.sleep(2000);
-        yearCalendar.click();
-        driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
+        // driver.findElement(By.cssSelector("input[data-date-format='Y-m-d']")).click();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].click();", driver.findElement(By.cssSelector("input[name='google_accounts.mb_delivery_date']")));
+        Thread.sleep(5000);
+        // Selectează toate elementele care respectă clasa
+List<WebElement> todayElements = driver.findElements(By.cssSelector("span.flatpickr-day.today"));
+System.out.println(todayElements);
+// Verifică dacă sunt suficiente elemente și selectează al 12-lea
+if (todayElements.size() >= 12) {
+    WebElement twelfthElement = todayElements.get(11); // Indexul este zero-based, deci al 12-lea este la index 11
+    twelfthElement.click();
+} else {
+    System.out.println("Nu există suficiente elemente today pentru a selecta al 12-lea.");
+}
 
-        List<WebElement> monthElements = driver.findElements(
-                By.cssSelector("button[class='react-calendar__tile react-calendar__year-view__months__month']"));
+        // WebElement yearCalendar = driver
+        //         .findElement(By.cssSelector("button[class ='react-calendar__navigation__label']"));
 
-        monthElements.get(Integer.parseInt(monthNumber) - 1).click();
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
+        // yearCalendar.click();
+        // Thread.sleep(2000);
+        // yearCalendar.click();
+        // driver.findElement(By.xpath("//button[text()='" + year + "']")).click();
 
-        List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+        // List<WebElement> monthElements = driver.findElements(
+        //         By.cssSelector("button[class='react-calendar__tile react-calendar__year-view__months__month']"));
 
-        for (int i = 0; i < actualList.size(); i++) {
-            System.out.println(actualList.get(i).getAttribute("value"));
-        }
+        // monthElements.get(Integer.parseInt(monthNumber) - 1).click();
+        // Thread.sleep(2000);
+        // driver.findElement(By.xpath("//abbr[text()='" + date + "']")).click();
+
+        // List<WebElement> actualList = driver.findElements(By.cssSelector(".react-date-picker__inputGroup__input"));
+
+        // for (int i = 0; i < actualList.size(); i++) {
+        //     System.out.println(actualList.get(i).getAttribute("value"));
+        // }
 }
 }
