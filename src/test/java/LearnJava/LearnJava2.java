b44a8ab5c40a567e571234c69be166680336f334
Victor Cristea <cristeavictor4@gmail.com>
2024-11-08 12:37:33 +0200

    dsadsa

--- src/test/java/LearnJava/LearnJava2.java
+++ src/test/java/LearnJava/LearnJava2.java
@@ -1,5 +1,7 @@
 package LearnJava;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.testng.annotations.Test;
@@ -10,7 +12,17 @@ public class LearnJava2 {
     public void streamMap(){
 
         // Print the names witch have last letter "a" with uppercase 
-        Stream.of("Andreea", "Bogdan", "Claudia", "Debil2").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+        Stream.of("Andreea", "Bogdana", "Claudia", "Debil2").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+        
+        // Prin names witch have first letter "a" with uppercase and sorted
+
+        List<String> names = Arrays.asList("Andreea", "Bogdana", "Claudia", "Debil2");
+
+        names.stream().filter(s->s.startsWith("A")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+
+        // Merge into arraylist
+
+        
 
     }
 }
