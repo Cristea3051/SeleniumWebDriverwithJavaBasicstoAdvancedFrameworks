e0ce5ccbe449d4a5048ba51cfa7c501458482fb4
Victor Cristea <cristeavictor4@gmail.com>
2024-11-08 17:25:23 +0200

    dsadasdsa

--- src/test/java/LearnJava/LearnJava2.java
+++ src/test/java/LearnJava/LearnJava2.java
@@ -1,9 +1,12 @@
 package LearnJava;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LearnJava2 {
@@ -12,17 +15,50 @@ public class LearnJava2 {
     public void streamMap(){
 
         // Print the names witch have last letter "a" with uppercase 
-        Stream.of("Andreea", "Bogdana", "Claudia", "Debil2").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
+        Stream.of("Andreea", "Bogdan", "Claudia", "Debil2").filter(s->s.endsWith("a")).map(s->s.toUpperCase()).forEach(s->System.out.println(s));
         
         // Prin names witch have first letter "a" with uppercase and sorted
 
-        List<String> names = Arrays.asList("Andreea", "Bogdana", "Claudia", "Debil2");
+        List<String> names = Arrays.asList("Andreea", "Bogdan", "Claudia", "Debil2");
 
         names.stream().filter(s->s.startsWith("A")).sorted().map(s->s.toUpperCase()).forEach(s->System.out.println(s));
 
-        // Merge into arraylist
+         // Merge into arraylist
+        //  Am doua arrays names1 si names, voi face un nou array cu elementele dina ceste doua
 
-        
+        ArrayList<String> brands = new ArrayList<String>();
 
+        brands.add("Asus");
+        brands.add("Apple");
+        brands.add("Razer");
+        brands.add("Lenovo");
+        brands.add("HP");
+        brands.add("MSI");
+        brands.add("Dell"); 
+
+      // Concatenez stream-urile și le colectez într-o listă pentru a putea folosi datele de mai multe ori
+    List<String> combinedList = Stream.concat(names.stream(), brands.stream()).sorted().toList(); 
+
+    // Printez și sortez toate elementele din lista nouă
+    combinedList.forEach(s -> System.out.println(s));
+
+    // Verific dacă lista conține elementul "Debil2"
+    boolean flag = combinedList.stream().anyMatch(s -> s.equalsIgnoreCase("Debil2"));
+    System.out.println(flag);
+    Assert.assertTrue(flag);
+
+    }
+
+    @Test
+    public void streamCollect(){
+
+        List<String> ls = Stream.of("Andreea", "Bogdan", "Claudia", "Debil2").filter(s ->s.endsWith("a"))
+        .map(s->s.toUpperCase()).collect(Collectors.toList());
+        System.out.println(ls);
+
+        List<Integer>numbers=Arrays.asList(1,2,3,4,4,5,5,1,4,5,6,7,8,9);
+        // Print unique nummber form the list
+        // Sort the array
+        numbers.stream().distinct().forEach(s->System.out.println(s));
     }
 }
