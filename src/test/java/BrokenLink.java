06245120f376fe26642337e006e3babe917fad69
Cristea3051 <cristeavictor4@gmail.com>
2024-11-11 14:36:39 +0200

    dsadsadsa

--- src/test/java/BrokenLink.java
+++ src/test/java/BrokenLink.java
@@ -1,9 +1,8 @@
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,41 +11,55 @@ import org.testng.asserts.SoftAssert;
 
 public class BrokenLink {
 
-    public static void main(String[] args) throws MalformedURLException, IOException {
+    public static void main(String[] args) throws IOException {
         
         WebDriver driver = new ChromeDriver();
-
-        // Broken URL 
         driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-
-
+        
+        // Obține toate link-urile de testat
         List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
         SoftAssert ass = new SoftAssert();
 
         for (WebElement link : links) {
-            // Extrage URL-ul din atributul "href" al fiecărui link
-            String url = link.getAttribute("href");
-        
-            // Creează o conexiune HTTP către acel URL
-            HttpURLConnection connection = (HttpURLConnection)new URL(url).openConnection();
-            connection.setRequestMethod("HEAD");
-            connection.connect();
-        
-            // Obține codul de răspuns HTTP
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code for URL: " + url + " ----- and name: " +  link.getText()  +" is " + responseCode);
-            // Verifică dacă răspunsul HTTP este de eroare (cod > 400)
-            ass.assertTrue(responseCode < 400, "The link with URL " + url + " is broken with response code: " + responseCode);
+            String urlString = link.getAttribute("href");
+            
+            try {
+                // Creează un URI și apoi convertește-l în URL
+                URI uri = URI.create(urlString);
+                URL url = uri.toURL();
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("HEAD");
+                connection.connect();
+                
+                int responseCode = connection.getResponseCode();
+                System.out.println("Response Code for URL: " + url + " ----- and name: " + link.getText() + " is " + responseCode);
+                
+                ass.assertTrue(responseCode < 400, "The link with URL " + url + " is broken with response code: " + responseCode);
+                
+            } catch (Exception e) {
+                System.out.println("Error with URL: " + urlString + " - " + e.getMessage());
+            }
         }
 
-        // Face print la statutul curent al paginii care este acesata
-        String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+        // Test pentru un link specific cu atributul 'href' care conține 'brokenlink'
+        String brokenLinkUrlString = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
+        try {
+            URI brokenLinkUri = URI.create(brokenLinkUrlString);
+            URL brokenLinkUrl = brokenLinkUri.toURL();
 
-        HttpURLConnection conn = (HttpURLConnection)new URL(url).openConnection();
-        conn.setRequestMethod("HEAD");
-        conn.connect();
-        int responseCode = conn.getResponseCode();
+            HttpURLConnection conn = (HttpURLConnection) brokenLinkUrl.openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            int responseCode = conn.getResponseCode();
 
-        System.out.println(responseCode);
+            System.out.println("Response code for broken link URL: " + brokenLinkUrl + " is " + responseCode);
+            
+        } catch (Exception e) {
+            System.out.println("Error with broken link URL: " + brokenLinkUrlString + " - " + e.getMessage());
+        }
+
+        driver.quit(); // Oprește driverul după rulare
+        ass.assertAll(); // Rapoartează toate aserțiile
     }
 }
--- src/test/java/CRMTest/SendHttpRequestToCrm.java
+++ src/test/java/CRMTest/SendHttpRequestToCrm.java
@@ -1,6 +1,7 @@
 package CRMTest;
 
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -12,8 +13,11 @@ public class SendHttpRequestToCrm {
         for (int i = 0; i < 200; i++) {
             executor.submit(() -> {
                 try {
-                    String url = "http://crm-dash/login"; // URL-ul de test
-                    HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+                    String urlString = "http://crm-dash/login"; // URL-ul de test
+                    URI uri = URI.create(urlString); // Creează URI
+                    URL url = uri.toURL(); // Convertește URI în URL
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                     conn.setRequestMethod("POST");
 
                     // Obținem și afișăm doar codul de răspuns
