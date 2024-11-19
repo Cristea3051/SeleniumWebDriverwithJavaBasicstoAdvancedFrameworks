d10493f1df394a31d270df69fd390462d8a3899e
Cristea3051 <cristeavictor4@gmail.com>
2024-11-19 13:53:21 +0200

    dsadsadas

--- src/test/java/RelativeLocators.java
+++ src/test/java/RelativeLocators.java
@@ -1,3 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
 public class RelativeLocators {
 
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://rahulshettyacademy.com/angularpractice/");
+
+        // Locate with above
+        WebElement nameEditbox = driver.findElement(By.cssSelector("[name='name']"));
+        System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditbox)).getText());
+
+        // Locate with below
+        WebElement dateBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+        driver.findElement(with(By.tagName("input")).below(dateBirth)).click();
+        
+
+        driver.quit();
+
+    }
 }
